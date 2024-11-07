9f1356ea1847c3203e1bbfd5880fcb47b32f6b5a
estherlardze <estherlardze8@gmail.com>
2024-11-07 05:53:04 +0000

    add update

--- week-1/to-do_list/todo.js
+++ week-1/to-do_list/todo.js
@@ -1,6 +1,11 @@
-const add = document.querySelector(".add");
-const todoForm = document.querySelector(".todo-form")
+const titleInput = document.querySelector(".title")
+const descriptionInput = document.querySelector(".description")
+const dateInput = document.querySelector("#date")
+const todoForm = document.querySelector(".form")
+const add = document.querySelector(".submit");
 const container = document.querySelector("#todo-container");
+
+const check = document.querySelector(".checkbox");
 
 
 let allTodos = [];
@@ -13,18 +18,23 @@ todoForm.addEventListener("submit", function(event) {
 
 // function to create a new todo
 function createTodo() {
-  const title = document.querySelector(".title").value;
-  const description = document.querySelector(".description").value;
-  const date = document.querySelector("#date").value;
+  const title = titleInput.value.trim();
+  const description = descriptionInput.value.trim();
+  const date = dateInput.value;
 
-  const todo = { title, description, date,};
-  if(title && description){
+  let todo = { title, description, date};
+
+  if(title && date){
     allTodos.push(todo);
+    console.log(allTodos);
     addTodo(todo)
-    title = "";
-    description = "";
-    date = "";
+
+    titleInput.value = "";
+    descriptionInput.value = "";
+    dateInput.value = "";
   }
+
+  saveTodos();
 }
 
 function addTodo(todo) {
@@ -37,15 +47,56 @@ div.innerHTML = `<div class="todo-item">
                         <img src="./edit.svg" alt="edit icon" class="icon edit">
                         <input type="checkbox" name="" id="">
                     </article>
-                    <p class="todo-description">${todo.description}</p>
+                    <p class="todo-description">${todo.description || ""}</p>
                     
                     <div class="todo-date">
                     <p class="">${todo.date}</p>
                     <p class="todo-time">2:00 pm</p>
                     </div>
                     </div>`;
-container.appendChild(div);
-      
+container.appendChild(div);     
 }
 
-add.addEventListener("click", createTodo);
+// mark todo as done
+
+
+  check.addEventListener("click", function(event) {
+    const checkbox = event.target;
+    const todoItem = checkbox.closest(".todo-item");
+    const todo = allTodos.find(todo => todoItem.dataset.id === todo.title);
+    todo.completed = checkbox.checked;
+    console.log(todo);
+  });
+
+  container.addEventListener("click", function(event) {
+    const target = event.target;
+    const todoItem = target.closest(".todo-item");
+    const todo = allTodos.find(todo => todoItem.dataset.id === todo.title);
+    if (target.classList.contains("delete")) {
+      allTodos = allTodos.filter(todo => todo.title !== todo.title);
+      todoItem.remove();
+    } else if (target.classList.contains("edit")) {
+      titleInput.value = todo.title;
+      descriptionInput.value = todo.description;
+      dateInput.value = todo.date;
+      allTodos = allTodos.filter(todo => todo.title !== todo.title);
+      todoItem.remove();
+    }
+    saveTodos();
+  });
+
+  // save todo
+
+  function saveTodos(){
+    localStorage.setItem("todos", container.innerHTML);
+  }
+
+  // load todos
+
+  function loadTodos(){
+    container.innerHTML = localStorage.getItem("todos");
+   
+  }
+
+  loadTodos()
+
